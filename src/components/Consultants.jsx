c6325ce3f51db2094c13733092363fa25b354ddb
Sushey01 <susmagar012@gmail.com>
2025-06-03 17:15:05 +0545

    mobile-responsive-left

--- src/components/Consultants.jsx
+++ src/components/Consultants.jsx
@@ -65,6 +65,33 @@ const Consultants = () => {
     slidesToScroll: 1,
     // prevArrow: <CustomPrevArrow />,
     // nextArrow: <CustomNextArrow />,
+
+    responsive: [
+        {
+            breakpoint:1024,
+            settings:{
+                slidesToShow:3,
+                slidesToScroll:1,
+                infinite:true,
+                dots:true,
+            }
+        },
+        {
+            breakpoint:600,
+            settings: {
+                slidesToShow:2,
+                slidesToScroll:1,
+                initialSlide:2
+            }
+        }, 
+        {
+            breakpoint:480,
+            settings: {
+                slidesToScroll:1,
+                slidesToShow:1
+            }
+        }
+    ]
   };
 
   return (
