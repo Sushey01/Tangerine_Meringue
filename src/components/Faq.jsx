02c50759c2d6b50f3f3e6cc3c6b4ef41257f9b90
Sushey01 <susmagar012@gmail.com>
2025-06-05 13:51:54 +0545

    solved

---
+++ src/components/Faq.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Faq = () => {
+  return (
+    <section className="section9">
+      <div className="faq-container">
+        <div className="faq-title">
+          <h1> FAQ</h1>
+          <p>
+            The history of hospital service in Nepal starts in the year 1889
+            with the establishment of South himal Hospital, the oldest hospital
+            now providing general care to tertiary care to the people of Nepal.
+            South himal hospital is the first teaching hospital for compounders,
+            dresser and later nurses
+          </p>
+        </div>
+
+        <div className="four-faq-container">
+          <div className="first-faq-container">
+            <h2>
+              The general surgery services are included under the Department of
+              surgery
+            </h2>
+          </div>
+          <div className="second-faq-container">
+            <h2>
+              The general surgery services are included under the Department of
+              surgery
+            </h2>
+          </div>
+          <div className="third-faq-container">
+            <h2>
+              The general surgery services are included under the Department of
+              surgery
+            </h2>
+          </div>
+          <div className="fourth-faq-container">
+            <h2>
+              The general surgery services are included under the Department of
+              surgery
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Faq;
