67f365799e4c1d2fae765d0970261aa8fa3a4dac
Sushey01 <susmagar012@gmail.com>
2025-06-06 17:17:12 +0545

    mobile-complete

--- src/App.jsx
+++ src/App.jsx
@@ -34,8 +34,8 @@ export default function App(){
       <OurCapacity/> 
        <NewsEvent/> 
        <Faq/> 
-      {/* <Testimonial/>
-      <Footer/> */}
+      <Testimonial/>
+      <Footer/>
     </div>
   )
 }
