e5932958cf053c98f8d320d61d8d85f68248278d
Sushey01 <susmagar012@gmail.com>
2025-06-11 13:40:56 +0545

    how

--- src/components/CapacityCard.jsx
+++ src/components/CapacityCard.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import './CapacityCard.css'
-import Capacity from '../assets/images/capacity.svg'
+import React from "react";
+import "./CapacityCard.css";
+import Capacity from "../assets/images/capacity.svg";
 
-
-const CapacityCard = ({image, title}) => {
+const CapacityCard = ({ image, title }) => {
   return (
     <div className="capacity-main-box">
-          <div className="capacity-box">
-                        <img src={image} alt={title}/>
-                        <div className="capacity-content">
-                             <p>25</p>
-                    <p>{title}</p>
-                        </div>
-                </div>
+      <div className="capacity-box">
+        <img src={image} alt={title} />
+        <div className="capacity-content">
+          <p>25</p>
+          <p>{title}</p>
+        </div>
+      </div>
     </div>
-
-  )
-}
+  );
+};
 
 export default CapacityCard;
